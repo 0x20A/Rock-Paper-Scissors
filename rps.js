6b7a73344822f647ab5fe3d258c5a72a1a78c5e1
0x20A <gustavojralonso0389@gmail.com>
2025-02-21 18:02:03 -0600

    styles added

--- rps.js
+++ rps.js
@@ -26,39 +26,39 @@
       // determine results
       if (playerMove === 'scissors'){
         if(computerMove === 'rock'){
-          result = 'You lose.';
+          result = 'You lose';
         } else if(computerMove === 'paper') {
-          result = 'You win.';
+          result = 'You win';
         } else if(computerMove === 'scissors') {
-          result = 'Tie.';
+          result = 'Tie';
         }
 
       } else if (playerMove === 'paper'){
         if(computerMove === 'rock'){
-          result = 'You win.';
+          result = 'You win';
         } else if(computerMove === 'paper') {
-          result = 'Tie.';
+          result = 'Tie';
         } else if(computerMove === 'scissors') {
-          result = 'You lose.';
+          result = 'You lose';
         }
 
       } else if (playerMove === 'rock'){
         if(computerMove === 'rock'){
-          result = 'Tie.';
+          result = 'Tie';
         } else if(computerMove === 'paper') {
-          result = 'You lose.';
+          result = 'You lose';
         } else if(computerMove === 'scissors') {
-          result = 'You win.';
+          result = 'You win';
         }
       }
 
       
       // update the scores
-      if (result === 'You win.') {
+      if (result === 'You win') {
         score.wins += 1;
-      } else if (result === 'You lose.') {
+      } else if (result === 'You lose') {
         score.loses += 1;
-      } else if (result === 'Tie.') {
+      } else if (result === 'Tie') {
         score.ties += 1;
       }
       
@@ -75,7 +75,8 @@
         .innerHTML = result;
 
       document.querySelector('.js-moves')
-        .innerHTML = `You picked ${playerMove}. Computer picked ${computerMove}.`;
+        .innerHTML = `You <img class="move-icon" src="img/${playerMove}-emoji.png" alt="">
+        <img class="move-icon" src="img/${computerMove}-emoji.png" alt=""> PC`;
 
 //       alert(`You picked ${playerMove}. Computer picked ${computerMove}. ${result}
 // Wins: ${score.wins}. Loses: ${score.loses}. Ties: ${score.ties}`);
@@ -83,7 +84,7 @@
 
     function updateScoreElement() {
       document.querySelector('.js-score')
-        .innerHTML = `Wins: ${score.wins}. Loses: ${score.loses}. Ties: ${score.ties}`;
+        .innerHTML = `Wins: ${score.wins} Loses: ${score.loses} Ties: ${score.ties}`;
     }
     
     function pickComputerMove(){
